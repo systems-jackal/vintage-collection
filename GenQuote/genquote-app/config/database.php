a2930e938b2804ee9257eb45e445b73add2fa6c1
systems-jackal <systemsjackal@gmail.com>
2026-06-13 17:20:12 -0400

    Replaced db configs tcp protocol to IPV4

--- GenQuote/genquote-app/config/database.php
+++ GenQuote/genquote-app/config/database.php
@@ -5,9 +5,12 @@ $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 
+// Resolve host to IPv4 address
+$ipv4 = gethostbyname($host); // This returns IPv4 if available
+
 try {
     $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname",
+        "pgsql:host=$ipv4;port=$port;dbname=$dbname",
         $user,
         $password,
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
